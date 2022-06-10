6a033427963dc9a6b2c9ffa0c7d7daec6d5c5813
odadki <93558662+odadki@users.noreply.github.com>
2022-06-10 11:15:24 -0700

    Added most front end files, up to Particles

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="f3">{"Danielle, your current rank is..."}</div>
+      <div className="f2">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
